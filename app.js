4423572f74d22ccd7c7c88720865206989993424
kingsiao <kingsolomonsiao@gmail.com>
2023-09-01 11:31:19 +0800

    Implemented CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -21,6 +22,10 @@ const bookingRouter = require('./routes/bookingRoutes');
 const app = express();
 
 app.enable('trust proxy');
+
+app.use(cors());
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
